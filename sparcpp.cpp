1daa80dd3df5586bc4356a6938e9c6ac2d4a38f6
scwatts <s.watts2@student.unimelb.edu.au>
2016-07-03 23:42:36 +1000

    Changed from 2-d to 1-d vector for otu observations when reading otu table

--- sparcpp.cpp
+++ sparcpp.cpp
@@ -11,7 +11,7 @@
 struct OtuTable {
     std::vector<std::string> sample_names;
     std::vector<std::string> otu_ids;
-    std::vector<std::vector<int>> otu_observations;
+    std::vector<int> otu_observations;
     int otu_number;
     int sample_number;
 };
@@ -26,6 +26,7 @@ struct OtuTable LoadOtuFile(std::string filename) {
     struct OtuTable otu_table;
     int otu_number = 0;
     int sample_number = 0;
+    bool id;
     // Open file stream
     std::ifstream otu_file;
     otu_file.open(filename);
@@ -47,11 +48,8 @@ struct OtuTable LoadOtuFile(std::string filename) {
     while(std::getline(otu_file, line)) {
         // TODO: Is there an alternate design pattern to loop variables as below
         // Loop variables
-        std::vector<int> otu_counts;
-        bool id;
         // (Re)sets variables for loop
         id = true;
-        otu_counts.clear();
         line_stream.clear();
         // Add current line to line stream and then split by tabs
         line_stream.str(line);
@@ -63,10 +61,8 @@ struct OtuTable LoadOtuFile(std::string filename) {
                 continue;
             }
             // Add current element to OTU count after converting to int
-            otu_counts.push_back(std::stoi(ele));
+            otu_table.otu_observations.push_back(std::stoi(ele));
         }
-        // Add current OTU counts to OtuTable struct instance
-        otu_table.otu_observations.push_back(otu_counts);
         ++otu_number;
     // TODO: Check if growing std::vector is sustainable for large tables
     }
@@ -85,7 +81,6 @@ arma::Mat<double> GetComponentFractions(struct OtuTable * otu_table, arma::Mat<i
     rng_type = gsl_rng_default;
     gsl_rng * p_rng = gsl_rng_alloc(gsl_rng_mt19937);
     gsl_rng_set(p_rng, time(NULL));
-
     // TODO: check if it's more efficient to gather fractions and then init arma::Mat (instead of init elements)
     // Estimate fractions by drawing from dirichlet distribution
     arma::Mat<double> fractions(otu_table->sample_number, otu_table->otu_number);
@@ -105,12 +100,13 @@ arma::Mat<double> GetComponentFractions(struct OtuTable * otu_table, arma::Mat<i
     }
     // Free rng memory
     gsl_rng_free(p_rng);
-
     return fractions;
 }
 
 
 arma::Mat<double> FromFileGetComponentFractions(struct OtuTable * otu_table, std::string filename) {
+    // NOTE: This is a temp function to provide a consistent fraction component estimatation. In final build,
+    // the function FileGetComponentFractions will be used.
     // Defining some variables
     std::string line;
     std::string ele;
@@ -140,22 +136,8 @@ int main() {
     std::string otu_filename;
     otu_filename = "fake_data.txt";
     struct OtuTable otu_table = LoadOtuFile(otu_filename);
-    // TODO: It appears from the armadillo docs you can initialise an arma:mat from a 1-d vector.
-    //       This means that when loading the OTU table, I could record the dimensions and have a
-    //       1-d std::vector<int>. For now I'll leave as is given that datastructure may be useful later
-    // Flatten std::vector of otu counts std::vector<double>
-    // TODO: Find the most suitable place for typedefs
-    typedef std::vector<std::vector<int>> count_container;
-    count_container * otu_observations = &otu_table.otu_observations;
-    std::vector<int> counts_vector;
-    for(count_container::iterator row = otu_observations->begin(); row != otu_observations->end(); ++row) {
-        for(std::vector<int>::iterator ele = (*row).begin(); ele != row->end(); ++ele){
-            // TODO: use a vector of int pointers rather than copying
-            counts_vector.push_back(*ele);
-        }
-    }
     // Construct count matrix
-    arma::Mat<int> counts(counts_vector);
+    arma::Mat<int> counts(otu_table.otu_observations);
     counts.reshape(otu_table.sample_number, otu_table.otu_number);
 
     // STEP 1: Estimate component fractions
