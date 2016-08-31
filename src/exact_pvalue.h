36d46a310be8ddb04e8590349a5031d093ff8e8b
scwatts <s.watts2@student.unimelb.edu.au>
2016-08-31 15:48:39 +1000

    Set permutation int to uint in exact_pvalues.cpp

--- src/exact_pvalue.h
+++ src/exact_pvalue.h
@@ -20,7 +20,7 @@ void count_values_more_extreme(arma::Mat<double>& abs_observed_correlation, arma
 double calculate_possbile_otu_permutations(std::unordered_map<double, int>& count_frequency, int sample_number);
 
 // Calculate the exact p-value (when total nperm <= 10000)
-double calculate_exact_pvalue(double otu_pair_possible_permutations, int& extreme_value_count, int& permutations);
+double calculate_exact_pvalue(double otu_pair_possible_permutations, int& extreme_value_count, unsigned int& permutations);
 
 // Calculate the p-value with an integral estimate (when total nperm > 10000)
 double calculate_pvalue_with_integral_estimate(double& otu_pair_possible_permutations, int& extreme_value_count, int& permutations);
