d6fff6945cf5072fa258544dc73e4b654cb60036
scwatts <s.watts2@student.unimelb.edu.au>
2017-05-29 12:39:22 +1000

    Updated brew formula

--- scripts/fastspar.rb
+++ scripts/fastspar.rb
@@ -1,8 +1,8 @@
 class Fastspar < Formula
   desc "A fast c++ implementation of SparCC"
   homepage "https://github.com/scwatts/fastspar"
-  url "https://github.com/scwatts/fastspar/archive/v0.0.3.tar.gz"
-  sha256 "5b9aab1e3a4e9935868a9b43cd93095d20daf37f9da569b51c4374af4cf24564"
+  url "https://github.com/scwatts/fastspar/archive/v0.0.4.tar.gz"
+  sha256 "64db95f37251031cfb757590477dc4d7545fa213ee08f0dc9560e72490cfb5f0"
   head "https://github.com/scwatts/fastspar.git"
 
   depends_on "armadillo" => :run
