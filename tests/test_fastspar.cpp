fbfe461d5c2aa2473e3a236cc6372458d4038fc9
scwatts <s.watts2@student.unimelb.edu.au>
2018-05-17 13:31:45 +1000

    Add simple integration test

--- tests/test_fastspar.cpp
+++ tests/test_fastspar.cpp
@@ -1,6 +1,29 @@
 #include "catch.hpp"
 
 
-TEST_CASE("dummy test") {
-    REQUIRE(true);
+#include "../src/fastspar.h"
+
+
+// Integration test
+// TODO: break into unittests
+TEST_CASE("Integration test") {
+    // Input files
+    std::string otu_fp("data/fake_data.tsv");
+    std::string correlation_fp("data/fake_data_cor.tsv");
+    std::string covariance_fp("data/fake_data_cov.tsv");
+
+    // Run correlations
+    OtuTable otu_table;
+    otu_table.load_otu_file(otu_fp);
+    FastSpar fastspar(&otu_table, 50, 10, 0.1, 1, 1);
+    fastspar.infer_correlation_and_covariance();
+    fastspar.calculate_median_correlation_and_covariance();
+
+    // Compare to previously validated outputs
+    arma::Mat<double> correlation = load_correlation_file(correlation_fp);
+    arma::Mat<double> covariance = load_correlation_file(covariance_fp);
+
+    // Tolerating difference of 0.0001 as output types are rounded
+    REQUIRE(arma::approx_equal(fastspar.median_correlation, correlation, "absdiff", 0.001));
+    REQUIRE(arma::approx_equal(fastspar.median_covariance, covariance, "absdiff", 0.001));
 }
