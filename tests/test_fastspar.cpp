bb4c436ab7f839f24f644328ff9925a74f4db9a2
scwatts <s.watts2@student.unimelb.edu.au>
2018-05-21 21:59:24 +1000

    Improve practical nature of tests

--- tests/test_fastspar.cpp
+++ tests/test_fastspar.cpp
@@ -1,7 +1,3 @@
-// Framework to provide main implementation
-#define CATCH_CONFIG_MAIN
-
-
 #include "catch.hpp"
 
 
---
+++ tests/test_main.cpp
@@ -0,0 +1,5 @@
+// Framework to provide main implementation
+#define CATCH_CONFIG_MAIN
+
+
+#include "catch.hpp"
